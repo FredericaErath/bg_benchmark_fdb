ae49ea0506c39c29dae993358a9386d19351fea5
Cassia <40779641+FredericaErath@users.noreply.github.com>
2025-05-06 19:48:37 -0700

    add new scripts and change get stats

--- src/edu/usc/bg/JanusGraphBGCoord.java
+++ src/edu/usc/bg/JanusGraphBGCoord.java
@@ -118,16 +118,16 @@ public class JanusGraphBGCoord {
         coord.isWrite = coord.ifWriteWorkload();
         // if is not write work load, load and warmup once
         // each rating experiment just need to load and do warmup once, then copy the backup file to current fdb
-//        if(coord.doLoad){
-//            coord.clearDBFDBManner();
-//            Process loadProcess = coord.loadDB();
-//
-//            String bgLoadLog = coord.watchProcessOutput(loadProcess,
-//                    "SHUTDOWN!!!",
-//                    "mainclass");
-//
-//            coord.saveToFile(directory + "/BGMainLoad-" + "0" +".log", bgLoadLog);
-//        }
+        if(coord.doLoad){
+            coord.clearDBFDBManner();
+            Process loadProcess = coord.loadDB();
+
+            String bgLoadLog = coord.watchProcessOutput(loadProcess,
+                    "SHUTDOWN!!!",
+                    "mainclass");
+
+            coord.saveToFile(directory + "/BGMainLoad-" + "0" +".log", bgLoadLog);
+        }
 
         if(coord.doWarmup){
             coord.warmUp(0);
